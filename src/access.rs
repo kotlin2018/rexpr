11a8e60baaba7901ea00268e36807b1665fae715
zhuxiujie <zhuxiujia@qq.com>
2021-01-25 18:27:22 +0800

    edit code

--- src/access.rs
+++ src/access.rs
@@ -12,25 +12,26 @@ pub trait AccessMethod {
 impl AccessField for Value {
     #[inline]
     fn access_field(&self, env: &Value) -> Result<Value, crate::error::Error> {
+        let null_value = Value::Null;
         let arr = self.as_array().unwrap();
         let arr_len = arr.len() as i32;
         if arr_len == 0 {
-            return Result::Ok(Value::Null);
+            return Result::Ok(null_value);
         }
         let mut index = 0;
         let mut v = env;
         for item in arr {
             let item_str = item.as_str().unwrap();
             if v.is_object() {
-                v = v.get(item_str).unwrap_or(&Value::Null);
+                v = v.get(item_str).unwrap_or(&null_value);
             } else if v.is_array() {
                 let item_index = item_str.parse::<usize>();
                 if item_index.is_err() {
-                    return Result::Ok(serde_json::Value::Null);
+                    return Result::Ok(null_value);
                 }
                 let item_index = item_index.unwrap();
                 let arr_ref = v.as_array().unwrap();
-                v = arr_ref.get(item_index).unwrap_or(&Value::Null);
+                v = arr_ref.get(item_index).unwrap_or(&null_value);
             }
             if v.is_null() || index + 1 >= arr_len {
                 return Result::Ok(v.clone());
