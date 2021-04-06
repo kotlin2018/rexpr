c7c6a42ffef3c22f6eb3d68559546a05cdf75308
zhuxiujie <zhuxiujia@qq.com>
2021-04-06 15:31:49 +0800

    clear code

--- example/src/main.rs
+++ example/src/main.rs
@@ -9,12 +9,27 @@ fn main() {
     println!("result: {}", result);
 }
 
-#[expr("1+2")]
-pub fn gen(arg: &mut serde_json::Value) -> rexpr::error::Result<serde_json::Value> {}
+macro_rules! attr {
+    () => {};
+}
+
+#[expr("1+2+&arg['a']['b'].as_i64().unwrap_or(0)")]
+pub fn gen(arg: &mut serde_json::Value) -> rexpr::error::Result<serde_json::Value> {
+    let a_b = &arg["a"]["b"];
+    if a_b.is_string() {
+        let mut owner1 = String::new();
+        owner1 = owner1 + "1" + "2" + a_b.as_str().unwrap_or("");
+    }
+    Ok(serde_json::json!(1+2+a_b.as_i64().unwrap_or(0)))
+}
 
 #[test]
 fn it_works() {
-    let mut arg = serde_json::json!({});
+    let mut arg = serde_json::json!({
+        "a":{
+            "b":8
+        }
+    });
     let v = gen(&mut arg);
     println!("{}", v.unwrap());
 }
--- rexpr-macro/src/func.rs
+++ rexpr-macro/src/func.rs
@@ -2,15 +2,18 @@ use proc_macro2::{Ident, Span};
 use quote::quote;
 use quote::ToTokens;
 use syn;
-use syn::{AttributeArgs, ItemFn, Expr, DeriveInput, parse_macro_input};
+use syn::{AttributeArgs, DeriveInput, Expr, ItemFn, parse_macro_input};
+use syn::parse::{Parse, ParseStream};
 
 use crate::proc_macro::TokenStream;
-use syn::parse::{ParseStream, Parse};
 
 pub(crate) fn impl_fn(f: &ItemFn, args: crate::proc_macro::TokenStream) -> TokenStream {
-    let s = syn::parse::<syn::LitStr>(args).unwrap();
-    let t = syn::parse_str::<Expr>(&s.value()).unwrap();
-    //println!("expr:{}", s.as_str());
+    let mut string_data = args.to_string();
+    string_data = string_data[1..string_data.len() - 1].to_string();
+    string_data = string_data.replace("'", "\"");
+    println!("string_data:{}", string_data);
+    //let s = syn::parse_str::<syn::LitStr>(&string_data).unwrap();
+    let t = syn::parse_str::<Expr>(&string_data).unwrap();
     let exprs = t.to_token_stream();
     return quote!(pub fn gen(arg:&mut serde_json::Value) -> rexpr::error::Result<serde_json::Value> {
                       let v=#t;
